0fed180cedf6e4170c448c8cf1f8433026f975d0
Scott Robertson <spr@scottr.org>
2014-06-14 16:20:08 -0700

    First Post

---
+++ June-2014/WaysToSort-ClosureExamples.playground/section-1.swift
@@ -0,0 +1,57 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+import Swift
+
+// A helper function for this example
+extension Array {
+    mutating func shuffle() {
+        for i in 0..self.count {
+            let swapIndex = Int(rand()) % self.count
+            let swapValue = self[swapIndex]
+            self[swapIndex] = self[i]
+            self[i] = swapValue
+        }
+    }
+}
+
+var numbers = [5, 6, 3, 2, 1, 4, 9, 0, 8, 7]
+
+// Verbose form
+numbers.sort({ (left, right) -> Bool in
+    return left < right
+})
+
+numbers.shuffle()
+numbers
+
+// Return value is implied due to type inference
+numbers.sort({ (left, right) in
+    return left < right
+})
+
+numbers.shuffle()
+numbers
+
+// Single line closures can omit "return"
+numbers.sort({ (left, right) in
+    left < right
+})
+
+numbers.shuffle()
+numbers
+
+// Swift has default variables for closures as well
+numbers.sort({ $0 < $1 })
+
+numbers.shuffle()
+numbers
+
+// Trailing closures can omit the ()
+numbers.sort { $0 < $1 }
+
+numbers.shuffle()
+numbers
+
+// Swift can figure out how to use the infix function "<" in this context as well
+numbers.sort(<)
--- README.md
+++ README.md
@@ -2,3 +2,13 @@ littleswiftsnippets
 ===================
 
 Swift Playgrounds from the Blog littleswiftsnippets.tumblr.com
+
+Layout
+------
+
+Each directory is tied to a month of posts on the blog. In it is a Swift Playground for each post.
+
+License
+-------
+
+Using the unlicense, please use these as you see fit.
